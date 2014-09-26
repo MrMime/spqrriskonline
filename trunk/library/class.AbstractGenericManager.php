a22734fb491b5a13186e50ae64d3a106436e95ab
amadei@gmail.com <amadei@gmail.com@35e7478f-f2fe-051a-3bc2-86c2b7a8563e>
2014-09-26 12:23:57 +0000

    Aggiunte query dinamiche

--- trunk/library/class.AbstractGenericManager.php
+++ trunk/library/class.AbstractGenericManager.php
@@ -4,13 +4,20 @@
 Abstract class AbstractGenericManager
 {
 	protected $_xml 	= null;
+	protected $_commonXml = null;
 	protected $_result 	= null;
 	protected $_db		= null;
 	
 	public function __construct(){
 		global $db;
 		$this->_db = $db;
+		$this->_commonXml = simplexml_load_file('./db/common.xml');
 	}
 	
+	protected function query($query,$parameters){
+		$query = str_replace("\n\n","\n",$query);
+		$query = trim($query);
+		return $this->_db->query($query,$parameters);
+	}
 	
 }
